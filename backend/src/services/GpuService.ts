41bfa8b833d7c91aabff983a52a02bdf7011da59
chaotic <chaotic@chaotics-MacBook-Pro.local>
2025-06-01 14:52:05 -0400

    fifth push, added more gpu related services

--- backend/src/services/GpuService.ts
+++ backend/src/services/GpuService.ts
@@ -36,3 +36,56 @@ export const getAllGpusService = async (): Promise<ResponseInterface> => {
         return { error: (e as Error).message }
     }
 }
+
+export const getGpuById = async (id: number): Promise<ResponseInterface> => {
+    try {
+        const gpu = await prisma.gPU.findFirst({
+            where: {
+                id: id
+            }
+        });
+
+        if (!gpu) {
+            return { error: "No GPU found" }
+        }
+        return { response: gpu }
+    } catch (e) {
+        return { error: (e as Error).message }
+    }
+}
+
+export const updateGpuById = async (id: number, gpu: GPUWriteDto): Promise<ResponseInterface> => {
+    try {
+        const getGpu = await getGpuById(id);
+
+        if (!getGpu) {
+            return { error: "No GPU found" }
+        }
+        const updateGpu = await prisma.gPU.update({
+            where: {
+                id: id
+            },
+            data:
+            {
+                name: gpu.name,
+                manufacturer: gpu.manufacturer
+            }
+        })
+        return { response: "gpu updated" }
+    } catch (e) {
+        return { error: (e as Error).message }
+    }
+}
+
+export const deleteGpuById = async (id: number): Promise<ResponseInterface> => {
+    try {
+        const gpu = await prisma.gPU.findUnique({ where: { id } });
+        if (!gpu) {
+            return { error: "No GPU found" };
+        }
+        await prisma.gPU.delete({ where: { id } });
+        return { response: "GPU deleted" };
+    } catch (e) {
+        return { error: (e as Error).message };
+    }
+};
